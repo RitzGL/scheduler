972dfd12a5998bde835d330ea6668416e04b426a
Ritz GL <maurigarzonlara@hotmail.com>
2021-04-08 22:57:15 +0800

    functionality and behaviour tested + refactor

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,16 @@
 // display the current day on the screen
 function displayCurrentDay(){
+
     // get the moment object, and format it with .format
     let currentDay = moment().format("dddd, MMM Do");
+
     // select currentDay id, set its content to 'now' variable
     $('#currentDay').text(currentDay);
 }
 
 // return the current hour from the moment.js API
 function getCurrentHour(){
+
     // get current hour from moment.js for comparison
     let currentHour = moment().format("H");
     
@@ -16,6 +19,7 @@ function getCurrentHour(){
 
 // get an array of selected elements 
 function getElementArray(str, el){
+
     // store the selected elements into an array
     let elementArray = $(str).find(el);
     
@@ -25,7 +29,9 @@ function getElementArray(str, el){
 // populate and return an array of strings
 // representing the hours on the page
 function getHoursFromPage(elementArray){
+
     let hours = [];
+
     // getting the text content of these elements into my hours[]
     for(let i = 0; i < elementArray.length; i++){
 
@@ -40,31 +46,36 @@ function convertTo24Hr(str){
     
     // evaluate string content
     if(str.endsWith("am") || str.startsWith("12")){
+
         // parse string to a number
         let number = parseInt(str, 10)
+
         // return the number when "am" is present or value is "12"
         return number;
+
     }else{
+
         let number = parseInt(str, 10)
+
         // return number in 24hr format
         return number + 12;
     }
  
 }
 
-// revert time from 24-hour clock, to 12-hour time
-function revertTime(time){
-    return time - 12;
-}
-
 // check if time is greater than, equal to or less than timeblock's time
 function compareTime(currentTime, pageTime, textArea){
     
     if(pageTime < currentTime){
+
         textArea.className = "col-8 past";
+
     }else if(pageTime == currentTime){
+
         textArea.className = "col-8 present"
+
     }else if(pageTime > currentTime){
+
         textArea.className = "col-8 future"
     }
     
@@ -76,55 +87,76 @@ let headerArray = getElementArray(".container","h4");
 // get the <textarea> elements from the page
 let textAreaArray = getElementArray(".container", "textarea");
 
+// get the <button> elements from the page
 let buttonArray = getElementArray(".container","button");
 
 // depending on current time, change colour of timeblock
 function changeTimeblockColour(){
 
-    console.log(textAreaArray);
-
     // get the textContent from the elements array
     let hoursTwentyFour = getHoursFromPage(headerArray);
 
     let currentTime = getCurrentHour();
-    // console.log(currentTime)
 
     for(let i=0;i<hoursTwentyFour.length; i++){
         
         hoursTwentyFour[i] = convertTo24Hr(hoursTwentyFour[i]);
+
         compareTime(currentTime, hoursTwentyFour[i], textAreaArray[i]);
 
     }
 
 }
 
-function storeEvent(){
-
-    // localStorage.clear()
+// store the textarea value in local storage
+function storeEvent(e){
+    // prevent the page from removing textarea values automatically on refresh
+    e.preventDefault();
 
     let textValue = []
+
+    // store textarea values into new array for storage
     for(i = 0; i < textAreaArray.length; i++){
+
         textValue[i] = textAreaArray[i].value
+
     }
-    
-    console.log(textValue)
 
     localStorage.setItem("Event",textValue)
     
 }
 
-console.log(buttonArray)
+// retrieve array of values from localStorage
+function retrieveEvent(){
 
-buttonArray.click(storeEvent)
+    // get the localStorage object, in this case a string
+    let stringValue = localStorage.getItem("Event")
+
+    return stringValue
+}
+
+// display the stored values from storage unto timeblock
+function displayEvent(){
+
+    // get the Storage object from localStorage
+    let retrievedObject = retrieveEvent()
+
+    // make new array from the Storage object (in this case a long string)
+    let arrayFromObject = retrievedObject.split(",")
+
+    // set the textarea value to the new array
+    for(let i = 0; i < arrayFromObject.length; i++){
+
+        textAreaArray[i].value = arrayFromObject[i]  
+
+    }
+
+}
+
+buttonArray.click(storeEvent);
+
+displayEvent();
 
 displayCurrentDay();
 
 changeTimeblockColour();
-
-
-// console.log(currentHour);
-// elementArray = $(".container").find("span");
-// console.log(hours)
-// array of span elements
-// console.log(elementArray);
-
